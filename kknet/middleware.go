a6f1b7e6a5bb74609787897c68812f7bc7e3168b
vsun <374708234@qq.com>
2026-01-18 21:39:28 +0800

    Update middleware.go

--- kknet/middleware.go
+++ kknet/middleware.go
@@ -1,61 +1,8 @@
 package kknet
 
-import (
-	"github.com/vvisun/kkdg/utils/buffers"
-)
-
 // Middleware is a function that wraps a IHandler to provide additional functionality.
 // It receives the next IHandler in the chain and returns a new IHandler.
 type Middleware func(IHandler) IHandler
-
-// middlewareChain chains multiple middlewares together.
-type middlewareChain struct {
-	middlewares []Middleware
-	handler     IHandler
-}
-
-// newMiddlewareChain creates a new middleware chain.
-func newMiddlewareChain(handler IHandler, middlewares []Middleware) IHandler {
-	if len(middlewares) == 0 {
-		return handler
-	}
-
-	// Apply middlewares in reverse order so the first middleware
-	// in the slice is the outermost wrapper.
-	h := handler
-	for i := len(middlewares) - 1; i >= 0; i-- {
-		if middlewares[i] != nil {
-			h = middlewares[i](h)
-		}
-	}
-	return h
-}
-
-// handlerWrapper wraps a Handler with middleware support.
-type handlerWrapper struct {
-	handler IHandler
-}
-
-// OnConnect implements Handler.
-func (w *handlerWrapper) OnConnect(c IConn) {
-	if w.handler != nil {
-		w.handler.OnConnect(c)
-	}
-}
-
-// OnMessage implements Handler.
-func (w *handlerWrapper) OnMessage(c IConn, data buffers.IBuffer) {
-	if w.handler != nil {
-		w.handler.OnMessage(c, data)
-	}
-}
-
-// OnClose implements Handler.
-func (w *handlerWrapper) OnClose(c IConn, err error) {
-	if w.handler != nil {
-		w.handler.OnClose(c, err)
-	}
-}
 
 // ApplyMiddlewares applies a chain of middlewares to a handler.
 // If handler is nil, returns nil.
@@ -67,5 +14,14 @@ func ApplyMiddlewares(handler IHandler, middlewares ...Middleware) IHandler {
 	if len(middlewares) == 0 {
 		return handler
 	}
-	return newMiddlewareChain(handler, middlewares)
+	// Apply in reverse order so the first middleware is the outermost wrapper.
+	h := handler
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		mw := middlewares[i]
+		if mw == nil {
+			continue
+		}
+		h = mw(h)
+	}
+	return h
 }
