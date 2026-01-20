b19e1c03a45e4a24fc14d320e8dc710b401c5c3b
vsun <374708234@qq.com>
2026-01-20 20:34:30 +0800

    移除protoactor

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.25.2
 
 require (
 	github.com/BurntSushi/toml v1.6.0
-	github.com/asynkron/protoactor-go v0.0.0-20260116191520-a3878dc4fe2d
 	github.com/bytedance/sonic v1.14.2
 	github.com/gorilla/websocket v1.5.3
 	github.com/nats-io/nats.go v1.48.0
@@ -14,39 +13,20 @@ require (
 )
 
 require (
-	github.com/Workiva/go-datastructures v1.1.6 // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/emirpasic/gods v1.18.1 // indirect
-	github.com/go-logr/logr v1.4.3 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/grafana/regexp v0.0.0-20250905093917-f7b3be9d1853 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/klauspost/compress v1.18.2 // indirect
-	github.com/lithammer/shortuuid/v4 v4.2.0 // indirect
-	github.com/lmittmann/tint v1.1.2 // indirect
-	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/nats-io/nkeys v0.4.12 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	github.com/orcaman/concurrent-map v1.0.0 // indirect
-	github.com/prometheus/client_golang v1.23.2 // indirect
-	github.com/prometheus/client_model v0.6.2 // indirect
-	github.com/prometheus/common v0.66.1 // indirect
-	github.com/prometheus/otlptranslator v0.0.2 // indirect
-	github.com/prometheus/procfs v0.17.0 // indirect
-	github.com/twmb/murmur3 v1.1.8 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
-	go.opentelemetry.io/otel v1.38.0 // indirect
-	go.opentelemetry.io/otel/exporters/prometheus v0.60.0 // indirect
-	go.opentelemetry.io/otel/metric v1.38.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.38.0 // indirect
-	go.opentelemetry.io/otel/sdk/metric v1.38.0 // indirect
-	go.opentelemetry.io/otel/trace v1.38.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	golang.org/x/crypto v0.46.0 // indirect
 	golang.org/x/sync v0.18.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
 
--- kknet/kkactor/actor.go
+++ kknet/kkactor/actor.go
@@ -2,10 +2,13 @@ package kkactor
 
 import (
 	"context"
+	"strconv"
+	"sync"
+	"sync/atomic"
 
-	"github.com/asynkron/protoactor-go/actor"
 	"github.com/vvisun/kkdg/kknet"
 	"github.com/vvisun/kkdg/utils/buffers"
+	"github.com/vvisun/kkdg/utils/kklog"
 )
 
 // ActorEventType represents connection event type.
@@ -17,7 +20,7 @@ const (
 	ActorEventClose
 )
 
-// ActorEvent is delivered to protoactor actors.
+// ActorEvent is delivered to actors.
 type ActorEvent struct {
 	Type ActorEventType
 	Conn kknet.IConn
@@ -25,18 +28,312 @@ type ActorEvent struct {
 	Err  error
 }
 
-// ActorHandler adapts a protoactor actor to a kknet Handler.
+// Context represents the actor context for receiving messages.
+type Context interface {
+	Message() interface{}
+	Self() *PID
+	Sender() *PID
+	Send(pid *PID, message interface{})
+	Stop(pid *PID)
+}
+
+// Actor is the interface that actors must implement.
+type Actor interface {
+	Receive(ctx Context)
+}
+
+// ActorFunc is a function that implements Actor.
+type ActorFunc func(ctx Context)
+
+// Receive implements Actor.
+func (f ActorFunc) Receive(ctx Context) {
+	f(ctx)
+}
+
+// PID represents a process ID (actor identifier).
+type PID struct {
+	id     string
+	system *ActorSystem
+}
+
+// String returns the string representation of the PID.
+func (pid *PID) String() string {
+	return pid.id
+}
+
+// Props represents actor properties/configuration.
+type Props struct {
+	actorProducer func() Actor
+}
+
+// PropsFromFunc creates Props from an ActorFunc.
+func PropsFromFunc(fn func(ctx Context)) *Props {
+	return &Props{
+		actorProducer: func() Actor {
+			return ActorFunc(fn)
+		},
+	}
+}
+
+// PropsFromProducer creates Props from an actor producer function.
+func PropsFromProducer(producer func() Actor) *Props {
+	return &Props{
+		actorProducer: producer,
+	}
+}
+
+// actorInstance represents a running actor instance.
+type actorInstance struct {
+	pid      *PID
+	actor    Actor
+	mailbox  chan interface{}
+	context  *actorContext
+	stopCh   chan struct{}
+	doneCh   chan struct{}
+	wg       sync.WaitGroup
+	stopping atomic.Bool
+}
+
+// actorContext implements Context interface.
+type actorContext struct {
+	instance *actorInstance
+	message  interface{}
+	sender   *PID
+}
+
+func (ctx *actorContext) Message() interface{} {
+	return ctx.message
+}
+
+func (ctx *actorContext) Self() *PID {
+	return ctx.instance.pid
+}
+
+func (ctx *actorContext) Sender() *PID {
+	return ctx.sender
+}
+
+func (ctx *actorContext) Send(pid *PID, message interface{}) {
+	if pid == nil || pid.system == nil {
+		return
+	}
+	pid.system.send(pid, message, nil)
+}
+
+func (ctx *actorContext) Stop(pid *PID) {
+	if pid == nil || pid.system == nil {
+		return
+	}
+	pid.system.stop(pid)
+}
+
+// ActorSystem manages actors.
+type ActorSystem struct {
+	mu      sync.RWMutex
+	actors  map[string]*actorInstance
+	nextID  atomic.Uint64
+	stopCh  chan struct{}
+	stopped atomic.Bool
+}
+
+// NewActorSystem creates a new actor system.
+func NewActorSystem() *ActorSystem {
+	return &ActorSystem{
+		actors: make(map[string]*actorInstance),
+		stopCh: make(chan struct{}),
+	}
+}
+
+// Spawn creates a new actor and returns its PID.
+func (sys *ActorSystem) Spawn(props *Props) *PID {
+	if props == nil || props.actorProducer == nil {
+		return nil
+	}
+
+	id := sys.nextID.Add(1)
+	pidID := "actor-" + strconv.FormatUint(id, 10)
+	pid := &PID{
+		id:     pidID,
+		system: sys,
+	}
+
+	instance := &actorInstance{
+		pid:     pid,
+		actor:   props.actorProducer(),
+		mailbox: make(chan interface{}, 100), // 缓冲通道，避免阻塞
+		stopCh:  make(chan struct{}),
+		doneCh:  make(chan struct{}),
+	}
+
+	instance.context = &actorContext{
+		instance: instance,
+	}
+
+	sys.mu.Lock()
+	sys.actors[pidID] = instance
+	sys.mu.Unlock()
+
+	// 启动 actor 的消息处理循环
+	instance.wg.Add(1)
+	go instance.run()
+
+	return pid
+}
+
+// send sends a message to an actor.
+func (sys *ActorSystem) send(pid *PID, message interface{}, sender *PID) {
+	if pid == nil || pid.system != sys {
+		return
+	}
+
+	sys.mu.RLock()
+	instance, exists := sys.actors[pid.id]
+	sys.mu.RUnlock()
+
+	if !exists {
+		return
+	}
+
+	// 非阻塞发送，如果邮箱满了就丢弃消息
+	select {
+	case instance.mailbox <- message:
+	default:
+		kklog.Warnf("Actor mailbox full, dropping message to %s", pid.id)
+	}
+}
+
+// stop stops an actor.
+func (sys *ActorSystem) stop(pid *PID) {
+	if pid == nil || pid.system != sys {
+		return
+	}
+
+	sys.mu.Lock()
+	instance, exists := sys.actors[pid.id]
+	if exists {
+		delete(sys.actors, pid.id)
+	}
+	sys.mu.Unlock()
+
+	if !exists {
+		return
+	}
+
+	if instance.stopping.Swap(true) {
+		return
+	}
+
+	close(instance.stopCh)
+	instance.wg.Wait()
+}
+
+// Stop stops all actors and the system.
+func (sys *ActorSystem) Stop() {
+	if sys.stopped.Swap(true) {
+		return
+	}
+
+	close(sys.stopCh)
+
+	sys.mu.Lock()
+	instances := make([]*actorInstance, 0, len(sys.actors))
+	for _, instance := range sys.actors {
+		instances = append(instances, instance)
+	}
+	sys.mu.Unlock()
+
+	for _, instance := range instances {
+		if instance.stopping.Swap(true) {
+			continue
+		}
+		close(instance.stopCh)
+	}
+
+	for _, instance := range instances {
+		instance.wg.Wait()
+	}
+}
+
+// Root returns the root context for sending messages.
+type RootContext struct {
+	system *ActorSystem
+}
+
+// Send sends a message to an actor.
+func (rc *RootContext) Send(pid *PID, message interface{}) {
+	if rc == nil || rc.system == nil {
+		return
+	}
+	rc.system.send(pid, message, nil)
+}
+
+// StopFuture stops an actor and returns a future that completes when the actor is stopped.
+func (rc *RootContext) StopFuture(pid *PID) *StopFuture {
+	if rc == nil || rc.system == nil {
+		return &StopFuture{done: make(chan error, 1)}
+	}
+
+	future := &StopFuture{
+		done: make(chan error, 1),
+	}
+
+	go func() {
+		rc.system.stop(pid)
+		future.done <- nil
+	}()
+
+	return future
+}
+
+// StopFuture represents a future for actor stop operation.
+type StopFuture struct {
+	done chan error
+}
+
+// Wait waits for the stop operation to complete.
+func (f *StopFuture) Wait() error {
+	if f == nil {
+		return nil
+	}
+	return <-f.done
+}
+
+// run runs the actor's message processing loop.
+func (inst *actorInstance) run() {
+	defer inst.wg.Done()
+	defer close(inst.doneCh)
+
+	for {
+		select {
+		case <-inst.stopCh:
+			return
+		case msg := <-inst.mailbox:
+			inst.context.message = msg
+			inst.context.sender = nil // 可以扩展支持 sender
+			func() {
+				defer func() {
+					if r := recover(); r != nil {
+						kklog.Errorf("Actor panic in %s: %v", inst.pid.id, r)
+					}
+				}()
+				inst.actor.Receive(inst.context)
+			}()
+		}
+	}
+}
+
+// ActorHandler adapts an actor to a kknet Handler.
 type ActorHandler struct {
-	system *actor.ActorSystem
-	root   *actor.RootContext
-	pid    *actor.PID
+	system *ActorSystem
+	root   *RootContext
+	pid    *PID
 }
 
 // NewActorHandler creates a handler that forwards events to the actor.
-func NewActorHandler(props *actor.Props) *ActorHandler {
-	sys := actor.NewActorSystem()
-	root := sys.Root
-	pid := root.Spawn(props)
+func NewActorHandler(props *Props) *ActorHandler {
+	sys := NewActorSystem()
+	root := &RootContext{system: sys}
+	pid := sys.Spawn(props)
 	return &ActorHandler{
 		system: sys,
 		root:   root,
@@ -45,7 +342,7 @@ func NewActorHandler(props *actor.Props) *ActorHandler {
 }
 
 // System returns the underlying actor system.
-func (h *ActorHandler) System() *actor.ActorSystem {
+func (h *ActorHandler) System() *ActorSystem {
 	return h.system
 }
 
@@ -66,6 +363,8 @@ func (h *ActorHandler) Stop(ctx context.Context) {
 	case <-ctx.Done():
 	case <-done:
 	}
+	// 停止整个系统
+	h.system.Stop()
 }
 
 // OnConnect implements Handler.
