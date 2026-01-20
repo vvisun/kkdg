8a2e64373fc267514999a0ae0ab867241209fa62
vsun <374708234@qq.com>
2026-01-20 16:07:31 +0800

    Update facade.go

--- kknet/facade.go
+++ kknet/facade.go
@@ -7,6 +7,13 @@ import (
 	"github.com/vvisun/kkdg/utils/buffers"
 )
 
+var connIDCounter atomic.Int64
+
+// NextConnID returns a unique connection id.
+func NextConnID() int64 {
+	return connIDCounter.Add(1)
+}
+
 // IConn represents a network connection.
 type IConn interface {
 	ID() int64                      //unique connection id
@@ -22,13 +29,6 @@ type IHandler interface {
 	OnConnect(c IConn)
 	OnMessage(c IConn, data buffers.IBuffer)
 	OnClose(c IConn, err error)
-}
-
-var connIDCounter atomic.Int64
-
-// NextConnID returns a unique connection id.
-func NextConnID() int64 {
-	return connIDCounter.Add(1)
 }
 
 // IServer represents a server.
