6b4f0df39f357649594ab2255c147805ea64813d
vsun <374708234@qq.com>
2026-01-22 11:11:09 +0800

    proto

---
+++ proto/gen.bat
@@ -0,0 +1,4 @@
+protoc.exe --go_out=./pbbase ./pbbase/*.proto
+protoc.exe --go_out=./pbcluster ./pbcluster/*.proto
+
+PAUSE
---
+++ proto/gen.sh
@@ -0,0 +1,3 @@
+./protoc --go_out=./pbrpc ./protorpc/*.proto
+./protoc --go_out=./pbmsg ./protomsg/*.proto
+
