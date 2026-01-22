a0c64eaa020444962fac8cff0828fec55fb0cba5
vsun <374708234@qq.com>
2026-01-22 11:38:35 +0800

    errs

--- kkerrors/err_app.go
+++ kkerrors/err_app.go
@@ -11,14 +11,4 @@ var (
 	ErrAppNotShutdown = errors.New("app is not shutdown")
 	// 应用已关闭
 	ErrAppShutdown = errors.New("app is already shutdown")
-	// 重复添加
-	ErrComponentAlreadyAdded = errors.New("component already added")
-	// 重复移除
-	ErrComponentAlreadyRemoved = errors.New("component already removed")
-	// 重复设置父组件
-	ErrComponentAlreadySetParent = errors.New("component already set parent")
-	// 重复获取父组件
-	ErrComponentAlreadyGetParent = errors.New("component already get parent")
-	// 重复获取子组件
-	ErrComponentAlreadyGetChildren = errors.New("component already get children")
 )
--- kkerrors/err_component.go
+++ kkerrors/err_component.go
@@ -12,4 +12,14 @@ var (
 	ErrComponentNotShutdown = errors.New("component is not shutdown")
 	// 组件已关闭
 	ErrComponentShutdown = errors.New("component is already shutdown")
+	// 重复添加
+	ErrComponentAlreadyAdded = errors.New("component already added")
+	// 重复移除
+	ErrComponentAlreadyRemoved = errors.New("component already removed")
+	// 重复设置父组件
+	ErrComponentAlreadySetParent = errors.New("component already set parent")
+	// 重复获取父组件
+	ErrComponentAlreadyGetParent = errors.New("component already get parent")
+	// 重复获取子组件
+	ErrComponentAlreadyGetChildren = errors.New("component already get children")
 )
