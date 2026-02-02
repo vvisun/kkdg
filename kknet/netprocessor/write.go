57c7846c0f255a36b36f954b974b76cee5e2e68c
vsun <374708234@qq.com>
2026-02-02 15:28:53 +0800

    Update write.go

--- kknet/netprocessor/write.go
+++ kknet/netprocessor/write.go
@@ -222,6 +222,7 @@ func (wp *WriteProcessor) writeLoop() {
 				break
 			}
 
+			// TODO: 失败目前直接丢弃，后续可以考虑重试/重新入队/...。
 			if _, err := wp.writeFn(wp.sendBatchBuffer, n); err != nil {
 				// 如果 writeFn 没有自己释放/清理，这里兜底释放，避免泄漏
 				wp.drainRelease(n)
