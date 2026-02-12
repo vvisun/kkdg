77cca0eb26f2458b086e6d482cc1dfe74f94d8a2
vsun <374708234@qq.com>
2026-02-12 23:10:17 +0800

    msgpack

--- kknet/kkrpc/codec.go
+++ kknet/kkrpc/codec.go
@@ -9,9 +9,9 @@ import (
 
 var (
 	// rpc用的编码器
-	frameCodec kkcodec.ICodec = kkcodec.GetCodec(kkcodec.CodecTypeMsgpack)
+	frameCodec kkcodec.ICodec = kkcodec.GetCodec(kkcodec.CodecTypeJson)
 	// rpc消息里的Data字段编码器
-	payloadCodec kkcodec.ICodec = kkcodec.GetCodec(kkcodec.CodecTypeMsgpack)
+	payloadCodec kkcodec.ICodec = kkcodec.GetCodec(kkcodec.CodecTypeJson)
 	// 网关消息使用的编码器. 需和客户端约定好编码器类型
 	// gatewayCodec kkcodec.ICodec = kkcodec.GetCodec(kkcodec.CodecTypeProtoBuf)
 )
--- kknet/kkrpc/z_bench_rpc_test.go
+++ kknet/kkrpc/z_bench_rpc_test.go
@@ -3,6 +3,8 @@ package kkrpc
 import (
 	"context"
 	"net"
+	"runtime"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -113,4 +115,75 @@ func Benchmark_EncodeRpcFrame(b *testing.B) {
 		}
 		kkbuffer.Put(bb)
 	}
+}
+
+// Benchmark_InvokeUnary_Parallel 多连接并发调用。每个 goroutine 独占一个 client，无共享连接。
+func Benchmark_InvokeUnary_Parallel(b *testing.B) {
+	ClearRpcManagerForTest()
+	RegisterReqRspMethod[testReq, testRsp]("testReqRsp")
+
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		b.Fatalf("listen: %v", err)
+	}
+	addr := ln.Addr().String()
+	_ = ln.Close()
+
+	rpcRouter := NewRpcReceiver()
+	RegistReqRspHandler(rpcRouter, "testReqRsp", func(ctx context.Context, msg *testReq, resp *testRsp) error {
+		resp.Code = 0
+		resp.Msg = "test success"
+		return nil
+	})
+
+	svr := NewServer(addr, kknet.DefaultOptions(), rpcRouter)
+	if err := svr.Start(); err != nil {
+		b.Fatalf("start server: %v", err)
+	}
+	defer svr.Stop()
+
+	numConns := runtime.GOMAXPROCS(0)
+	if numConns < 4 {
+		numConns = 4
+	}
+	if numConns > 32 {
+		numConns = 32
+	}
+
+	clients := make([]*Client, numConns)
+	invokers := make([]ReqRspInvoker[testReq, testRsp], numConns)
+
+	for i := 0; i < numConns; i++ {
+		cli := NewClient(addr, kknet.DefaultOptions(), rpcRouter)
+		if err := cli.Start(); err != nil {
+			b.Fatalf("start client: %v", err)
+		}
+		clients[i] = cli
+		invokers[i] = NewReqRspInvoker[testReq, testRsp](cli, 0)
+	}
+	defer func() {
+		for _, c := range clients {
+			c.Stop()
+		}
+	}()
+
+	time.Sleep(200 * time.Millisecond)
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	var parallelLaneIdx uint32
+	b.RunParallel(func(pb *testing.PB) {
+		// 每个 goroutine 绑定到固定 client，避免共享连接
+		myLane := int(atomic.AddUint32(&parallelLaneIdx, 1)-1) % numConns
+		invoker := invokers[myLane]
+		i := 0
+		for pb.Next() {
+			req := testReq{ID: i, Data: "test"}
+			var resp testRsp
+			if err := invoker.Invoke(context.Background(), "testReqRsp", &req, CallConfig{}, &resp); err != nil {
+				b.Fatalf("invoke: %v", err)
+			}
+			i++
+		}
+	})
 }
