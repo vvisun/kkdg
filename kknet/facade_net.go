5b451247011e2ea3b27560e13dd10e3555b95253
vsun <374708234@qq.com>
2026-02-12 12:48:59 +0800

    Update facade_net.go

--- kknet/facade_net.go
+++ kknet/facade_net.go
@@ -16,8 +16,16 @@ type USER_ID = int64
 // counter for connection ID. unique id for the connection.
 var connIDCounter atomic.Uint64
 
+const maxUint64 = ^uint64(0)
+
 // NextConnID returns a unique connection ID.
 func NextConnID() CONN_ID {
+	// 如果超过uint64最大值，则重置为0。理论上不可能，但以防万一。
+	// 基本上达到uint64最大值，即使每秒1000万个连接，也需要几十年，
+	// 这时候1~几亿的connId基本上必然已经断开逻辑也已经清理了，不存在逻辑向死亡的connId发消息的情况。
+	if connIDCounter.Load() >= maxUint64 {
+		connIDCounter.Store(0)
+	}
 	return connIDCounter.Add(1)
 }
 
