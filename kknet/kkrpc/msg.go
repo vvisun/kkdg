d07faea3b2979f4cbce89e576179482d8011740f
vsun <374708234@qq.com>
2026-02-13 10:22:23 +0800

    Update msg.go

--- kknet/kkrpc/msg.go
+++ kknet/kkrpc/msg.go
@@ -1,8 +1,6 @@
 package kkrpc
 
-import "github.com/vvisun/kkdg/proto/pbrpc"
-
-type FrameType = uint32
+type FrameType uint8
 
 const (
 	FrameTypeUnknown       FrameType = 0
@@ -13,12 +11,26 @@ const (
 	FrameTypeTransBroadMsg FrameType = 5
 )
 
-type Frame = pbrpc.Frame
+type Frame struct {
+	T    FrameType `json:"t" msgpack:"t"`             // FrameType
+	ID   uint64    `json:"id" msgpack:"id"`           // request id. 0 means no response required
+	DL   int64     `json:"dl,omitempty" msgpack:"dl"` // deadline unix ms (0 means no deadline)
+	M    string    `json:"m,omitempty" msgpack:"m"`   // method（远程方法名）
+	P    []byte    `json:"p,omitempty" msgpack:"p"`   // payload（远程方法参数）
+	Code int32     `json:"c,omitempty" msgpack:"c"`   // status code (0 ok) 错误码
+	Err  string    `json:"e,omitempty" msgpack:"e"`   // error message 错误信息
+}
 
 type (
 	// TransMsg 网关转发消息
-	TransMsg = pbrpc.TransMsg
+	TransMsg struct {
+		ClientId int64  // 客户端id(connID)
+		Data     []byte // 转发数据
+	}
 
 	// TransBroadcast 网关转发群发消息
-	TransBroadMsg = pbrpc.TransBroadMsg
+	TransBroadMsg struct {
+		Clients []int64 // 客户端id列表(connID列表)
+		Data    []byte  // 转发数据
+	}
 )
