919ca3afcd26e04e2df4e4ce7385c7aa64a472e4
vsun <374708234@qq.com>
2026-03-05 17:34:58 +0800

    工具

--- other/examples/examapp/ptoexam/init.go
+++ other/examples/examapp/ptoexam/init.go
@@ -3,7 +3,12 @@ package ptoexam
 import "github.com/vvisun/kkdg/kknet/kkpacket"
 
 func InitMsgs(router *kkpacket.MsgRouter) {
+	// test.proto
 	router.Register(1, &Msg1Req{}, "logic")
 	router.Register(2, &Msg1Resp{}, "logic")
 	router.Register(3, &Msg2Broadcast{}, "logic")
+	// test1.proto
+	router.Register(1000, &AaaaReq{}, "combat")
+	router.Register(1001, &AaaaResp{}, "combat")
+	router.Register(1002, &BbbbResp{}, "combat")
 }
---
+++ tools/cmd/msdid/main.go
@@ -0,0 +1,8 @@
+package main
+
+// 该工具用于生成消息ID与消息类型的映射关系
+// 读取指定proto目录下的所有proto文件，根据route_cfg.json文件生成消息ID与消息类型的映射关系，输出到指定文件
+// eg: 读取"ptoexam"目录下的所有proto文件，根据"ptoexam/route_cfg.json"文件生成消息ID与消息类型的映射关系，输出到"ptoexam/init.go"文件
+func main() {
+
+}
