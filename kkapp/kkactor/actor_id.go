ce89607b97877e98107f2f5963b5e8be720dc3fa
vsun <374708234@qq.com>
2026-03-10 17:28:23 +0800

    Update actor_id.go

--- kkapp/kkactor/actor_id.go
+++ kkapp/kkactor/actor_id.go
@@ -8,9 +8,8 @@ const ActorKeySeparator = "/"
 // 不需要关心 Actor 所在节点，由 ActorLocator 自动判断本地/远程：
 // 如果【NodeID 为空字符串】或【NodeID 在当前进程的任意节点中存在】，则认为是本地 Actor；否则为远程 Actor。
 type LucencyActorID struct {
-	nodeID    string // 节点ID，如 game1、game2。为空表示本地Actor。
-	actorKey  string // actor 标识，如 "ccgame/main"、"gate/router"
-	actorName string // actor 名称。//缓存以优化性能
+	nodeID   string // 节点ID，如 game1、game2。为空表示本地Actor。
+	actorKey string // actor 标识，如 "ccgame/main"、"gate/router"
 }
 
 // NodeID 返回逻辑节点 ID。
@@ -23,17 +22,11 @@ func (id LucencyActorID) ActorKey() string {
 	return id.actorKey
 }
 
-// ActorName 返回 actor 名称。
-func (id LucencyActorID) ActorName() string {
-	return id.actorName
-}
-
 func NewLucencyActorID(nodeId, actorKey string) LucencyActorID {
 	id := LucencyActorID{
 		nodeID:   nodeId,
 		actorKey: actorKey,
 	}
-	id.actorName = GetActorName(id) //缓存以优化性能
 	return id
 }
 
