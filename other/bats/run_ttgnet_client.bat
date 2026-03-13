cdedd4aa34448f71ad376fc5ebcdb8e93b37bd00
vsun <374708234@qq.com>
2026-03-13 13:55:01 +0800

    s

--- other/bats/run_ttgnet_client.bat
+++ other/bats/run_ttgnet_client.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %~dp0\..
 rem conn 连接数  size 每条消息大小（字节） interval 每连接发送间隔  connDelay 建连间隔
-go run ./other/tests/ttgnet/ttgnetclient -addr=127.0.0.1:19190 -conn=80000 -size=222 -interval=3200ms -connDelay=2ms
+go run ../other/tests/ttgnet/ttgnetclient -addr=127.0.0.1:19190 -conn=80000 -size=222 -interval=3200ms -connDelay=2ms
 PAUSE
