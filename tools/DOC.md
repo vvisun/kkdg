bf6395c31d66f440e141f3d7c873cec666564c67
vsun <374708234@qq.com>
2026-03-13 13:41:39 +0800

    doc

---
+++ tools/DOC.md
@@ -0,0 +1,7 @@
+# tools目录下提供一些工具函数和工具包
+
+## cmd目录下是各种命令行工具
+
+1. fbspb 提供 FlatBuffers 与 Protobuf 的互相转换及代码生成
+2. fbs2struct 从 .fbs 文件生成 Go struct 及 Pack/UnmarshalFlatBuffer 方法
+3. msdid 用于生成消息ID与消息类型的映射关系, 生成自动化代码，减少人工编写代码的工作量
--- tools/fbspb/doc.go
+++ tools/fbspb/doc.go
@@ -5,5 +5,7 @@
 //	go run ./tools/cmd/fbspb fbs2proto path/to/file.fbs
 //	go run ./tools/cmd/fbspb proto2fbs path/to/file.proto
 //
-// 目录（递归）：将 path 换为目录即可。生成 FlatBuffers 代码需系统已安装 flatc：https://github.com/google/flatbuffers/releases
+// 目录（递归）：将 path 换为目录即可。
+// 生成 FlatBuffers 代码需系统已安装 flatc。
+// 安装方法：https://github.com/google/flatbuffers/releases
 package fbspb
