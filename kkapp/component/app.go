0cf13fc42fd9818a5bdddc57b5a91c9d1187ce72
vsun <374708234@qq.com>
2026-03-19 16:22:19 +0800

    Update app.go

--- kkapp/component/app.go
+++ kkapp/component/app.go
@@ -420,6 +420,7 @@ func (slf *Application) onComponentFault(ctx actor.Context, eData *faultreport.C
 		// 组件已经终止，这里无需额外应用级动作。
 	case faultreport.FaultActionRestartComp:
 		// 当前策略不做组件自动重启，后续可在这里接入重启编排。
+		kklog.Warnf("%s restart comp %s is not supported now", slf.logTag(), eData.ComponentName)
 	default:
 		kklog.Warnf("%s unknown fault action %d for component %s", slf.logTag(), action, eData.ComponentName)
 	}
