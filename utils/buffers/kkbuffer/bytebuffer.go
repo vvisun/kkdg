87399d7d7f676306c793008b13360b20e00c1292
vsun <374708234@qq.com>
2026-03-20 12:21:40 +0800

    docs

--- utils/buffers/kkbuffer/bytebuffer.go
+++ utils/buffers/kkbuffer/bytebuffer.go
@@ -5,15 +5,9 @@ import (
 	"sync/atomic"
 )
 
-// ByteBuffer provides byte buffer, which can be used for minimizing
-// memory allocations.
-//
-// ByteBuffer may be used with functions appending data to the given []byte
-// slice. See example code for details.
-//
-// Use Get for obtaining an empty byte buffer.
-// 使用原则: 在任何函数中，如果不再使用(不作为参数传递给其他函数，或不作为返回值时)则kkbuffer.Put()释放。
-// 如果传递给其他函数使用，不要释放，因为其他函数可能是异步使用，释放会引起数据错乱。
+// 使用原则:
+//  1. 在任何函数中，如果不再使用(不作为参数传递给其他函数，或不作为返回值时)则kkbuffer.Put()释放。
+//  2. 如果传递给其他函数使用，不要释放，因为其他函数可能是异步使用，释放会引起数据错乱。
 type ByteBuffer struct {
 	//是否已释放标志，防止重复释放
 	released atomic.Bool
