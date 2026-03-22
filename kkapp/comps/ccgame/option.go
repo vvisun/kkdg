8e39325055b4df4f871a3542f80a2846c2d4013f
vsun <374708234@qq.com>
2026-03-22 22:43:31 +0800

    Update option.go

--- kkapp/comps/ccgame/option.go
+++ kkapp/comps/ccgame/option.go
@@ -21,6 +21,9 @@ type Options struct {
 	// 集群服务器URL。TransType为TransTypeNats时可选设置。
 	ClusterOpts kkcluster.ClusterOption
 	// 会话管理器工作线程数量。会创建多个工作线程来解码消息。
+	// 默认值为 CPU 核心数 * 64。
+	// 单逻辑服用户数限制较高时，可以适当调大，实现采用的是WorkerQueue，内存开销基本可以忽略，且不会固定占用携程，消费完携程自动结束。
+	// 稍大可以减小多用户排队等待的公平性。
 	SessionManagerWorkersCount int
 	// 用于game服的会话消息接收器中。gametrans.ISessionMsgReceiver.OnSession中使用。
 	// 解码错误或消息ID不存在时，是否抛给上层处理。
@@ -43,7 +46,7 @@ func validateOption(opt *Options) error {
 		}
 	}
 	if opt.SessionManagerWorkersCount <= 0 {
-		opt.SessionManagerWorkersCount = xos.NumCPU() * 4
+		opt.SessionManagerWorkersCount = xos.NumCPU() * 64
 	}
 	return nil
 }
@@ -51,7 +54,7 @@ func validateOption(opt *Options) error {
 func DefaultOptions() Options {
 	return Options{
 		TransType:                  transport.TransTypeShard,
-		SessionManagerWorkersCount: xos.NumCPU() * 4,
+		SessionManagerWorkersCount: xos.NumCPU() * 64,
 	}
 }
 
