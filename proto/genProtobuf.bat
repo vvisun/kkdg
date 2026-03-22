68576bd2e27de8cc2eb233307b57561ecd16397f
vsun <374708234@qq.com>
2026-03-22 16:31:39 +0800

    pb

--- proto/genProtobuf.bat
+++ proto/genProtobuf.bat
@@ -1,5 +1,7 @@
-protoc.exe --go_out=./pbbase ./pbbase/*.proto
 protoc.exe --go_out=./pbcluster ./pbcluster/*.proto
 protoc.exe --go_out=./pbrpc ./pbrpc/*.proto
+rem pto.proto 不在 proto\ 下：须指定 --proto_path 为 .proto 所在目录；文件名写该目录下的相对路径（如 pto.proto）
+rem 避免使用 -I=../...（PowerShell 会误解析）；用 --proto_path= 更稳
+protoc.exe --proto_path=../kkapp/transport/ptotrans --go_out=paths=source_relative:../kkapp/transport/ptotrans pto.proto
 
 PAUSE
--- proto/genProtobuf.sh
+++ proto/genProtobuf.sh
@@ -1,4 +1,4 @@
-./protoc --go_out=./pbbase ./pbbase/*.proto
 ./protoc --go_out=./pbcluster ./pbcluster/*.proto
 ./protoc --go_out=./pbrpc ./pbrpc/*.proto
-
+# pto.proto lives under kkapp/transport/ptotrans: -I must be that dir; pass paths relative to -I
+./protoc --proto_path=../kkapp/transport/ptotrans --go_out=paths=source_relative:../kkapp/transport/ptotrans pto.proto
