0e183274e4a17ef121a11ead5ee6e2f90d7719fb
vsun <374708234@qq.com>
2026-03-24 18:19:27 +0800

    Update read_sync.go

--- kknet/kkprocessor/read_sync.go
+++ kknet/kkprocessor/read_sync.go
@@ -48,8 +48,11 @@ func NewSyncReadProcessor(opts kknet.ReadOptions) kknet.IReadProcessor {
 func (rp *SyncReadProcessor) Pending() int { return 0 }
 
 func (rp *SyncReadProcessor) Start(conn kknet.IConn) {
-	rp.closing.Store(false)
 	rp.conn = conn
+	if conn == nil {
+		return
+	}
+	rp.closing.Store(false)
 	rp.connID = conn.ID()
 }
 
